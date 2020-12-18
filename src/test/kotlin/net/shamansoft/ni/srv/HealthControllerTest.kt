46c3fa320cdfa01257f163616b16bd4971b35b4f
Alexey Khisamutdinov <25253567+khisamutdinov@users.noreply.github.com>
2020-12-17 16:18:36 -0800

    use assertk for tests assertions

--- src/test/kotlin/net/shamansoft/ni/srv/HealthControllerTest.kt
+++ src/test/kotlin/net/shamansoft/ni/srv/HealthControllerTest.kt
@@ -1,11 +1,12 @@
 package net.shamansoft.ni.srv
 
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isNotNull
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.client.RxHttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import javax.inject.Inject
 
@@ -18,16 +19,16 @@ class HealthControllerTest {
     @Test
     fun testHello() {
         val request: HttpRequest<Any> = HttpRequest.GET("/hello") // <3>
-        val body = client.toBlocking().retrieve(request)
-        assertNotNull(body)
-        assertEquals("Hello World from net.shamansoft.ni.srv.HealthController", body)
+        assertThat(client.toBlocking().retrieve(request))
+                .isNotNull()
+                .isEqualTo("Hello World from net.shamansoft.ni.srv.HealthController")
     }
 
     @Test
     fun testHealth() {
         val request: HttpRequest<Any> = HttpRequest.GET("/health")
-        val actual = client.toBlocking().retrieve(request)
-        assertNotNull(actual)
-        assertEquals("OK", actual)
+        assertThat(client.toBlocking().retrieve(request))
+                .isNotNull()
+                .isEqualTo("OK")
     }
 }
